f81c2428e7f39c9ec302e9b7e97f50afb6e4f794
Jade Hua <jadehua@berkeley.edu>
2015-07-05 23:14:17 -0700

    updated experience section with flip cards!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
   $(function() {
+    $("#sfdc").flip();
+    $("#ibm").flip();
+    $("#lhs").flip();
+    $("#break").flip();
+    $(".card").flip({
+      trigger: 'hover'
+    });
+
+  });
+
+
+  
+
+  $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
